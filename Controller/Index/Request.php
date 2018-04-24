5ec212210dcf7032d622ec2292f31d21affc1244
Alina Vasilevich <alina@convert.no>
2018-04-24 10:54:14 +0300

    Removed form key validation at checkout

--- Controller/Index/Request.php
+++ Controller/Index/Request.php
@@ -6,21 +6,10 @@ use Magento\Framework\DataObject;
 
 class Request extends \Dibs\Flexwin\Controller\Index
 {
-    protected $formKeyValidator;
-
-    public function __construct(\Magento\Framework\App\Action\Context $context,
-                                \Magento\Sales\Model\Order $order,
-                                \Magento\Quote\Model\Quote $quote,
-                                \Magento\Checkout\Model\Session $checkoutSession,
-                                \Dibs\Flexwin\Model\Method $method,
-                                \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator) {
-        parent::__construct($context, $order, $quote, $checkoutSession, $method);
-        $this->formKeyValidator = $formKeyValidator;
-    }
 
     public function execute()
     {
-        if ($this->checkPost() && $this->formKeyValidator->validate($this->_request)) {
+        if ($this->checkPost()) {
             $result = new DataObject();
             $response = $this->getResponse();
             $result->addData($this->method->collectRequestParams());
